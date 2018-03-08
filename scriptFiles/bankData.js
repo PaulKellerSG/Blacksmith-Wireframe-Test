71c19a94c47604ccb28dca1a84640da819ea23d8
Paul Keller <paul.keller.it@gmail.com>
2018-03-08 18:07:55 +0800

    Complete Re-write is under way.

---
+++ scriptFiles/bankData.js
@@ -0,0 +1,220 @@
+getBankData([
+    {
+        "bankName": "Blacksmith Bank",
+        "bankLogoName": "../images/swords-crossed.png",
+        "customerType": "Regulated FI",
+        "country": "SINGAPORE",
+        "regulator": "Monetary Authority of Singapore",
+        "industry": "Banking",
+        "products": [
+            {
+                "name": "Retail Banking",
+                "eddTrigger": "false"
+            },
+            {
+                "name": "Savings",
+                "eddTrigger": "false"
+            },
+            {
+                "name": "Loans",
+                "eddTrigger": "false"
+            }
+        ],
+        "alertFlags": [
+            {
+                "alertflagName": "Ultra High Risk Country Involvement",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Sanctions Targets",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Politically Exposed Persons",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Bearer Shares",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Adverse News",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Anonymous Accounts",
+                "flagValue": "false"
+            }
+        ],
+        "highRiskEDDTriggers": [
+            {
+                "triggerName": "High Profile Customer",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Nominee Shareholder",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Personal Asset Holdings",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "'Gatekeeper' Accounts",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Unusual Ownership Structure",
+                "triggerValue": "false"
+            }
+        ]
+    },
+    {
+        "bankName": "YFGC Bank",
+        "bankLogoName": "../images/general_bank_colour_2.png",
+        "customerType": "Regulated FI",
+        "country": "AUSTRALIA",
+        "regulator": "Monetary Authority of Australia",
+        "industry": "Banking",
+        "products": [
+            {
+                "name": "Trade Finance",
+                "eddTrigger": "true"
+            },
+            {
+                "name": "Retail Banking",
+                "eddTrigger": "false"
+            },
+            {
+                "name": "Savings",
+                "eddTrigger": "false"
+            },
+            {
+                "name": "Loans",
+                "eddTrigger": "false"
+            }
+        ],
+        "alertFlags": [
+            {
+                "alertflagName": "Ultra High Risk Country Involvement",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Sanctions Targets",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Politically Exposed Persons",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Bearer Shares",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Adverse News",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Anonymous Accounts",
+                "flagValue": "false"
+            }
+        ],
+        "highRiskEDDTriggers": [
+            {
+                "triggerName": "High Profile Customer",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Nominee Shareholder",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Personal Asset Holdings",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "'Gatekeeper' Accounts",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Unusual Ownership Structure",
+                "triggerValue": "false"
+            }
+        ]
+    },
+    {
+        "bankName": "Bank of Narnia",
+        "bankLogoName": "../images/venom-head.png",
+        "customerType": "Regulated FI",
+        "country": "Narnia",
+        "regulator": "Monetary Authority of Narnia",
+        "industry": "Banking",
+        "products": [
+            {
+                "name": "Trade Finance",
+                "eddTrigger": "true"
+            },
+            {
+                "name": "Correspondent Banking",
+                "eddTrigger": "true"
+            },
+            {
+                "name": "Real Estate",
+                "eddTrigger": "true"
+            },
+            {
+                "name": "Loans",
+                "eddTrigger": "false"
+            }
+        ],
+        "alertFlags": [
+            {
+                "alertflagName": "Ultra High Risk Country Involvement",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Sanctions Targets",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Politically Exposed Persons",
+                "flagValue": "true"
+            },
+            {
+                "alertflagName": "Bearer Shares",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Adverse News",
+                "flagValue": "false"
+            },
+            {
+                "alertflagName": "Anonymous Accounts",
+                "flagValue": "false"
+            }
+        ],
+        "highRiskEDDTriggers": [
+            {
+                "triggerName": "High Profile Customer",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Nominee Shareholder",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Personal Asset Holdings",
+                "triggerValue": "true"
+            },
+            {
+                "triggerName": "'Gatekeeper' Accounts",
+                "triggerValue": "false"
+            },
+            {
+                "triggerName": "Unusual Ownership Structure",
+                "triggerValue": "false"
+            }
+        ]
+    }
+]);
