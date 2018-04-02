36742d03762ef2c5b5e25f588b6cdaf08a082e3a
Paul Keller <paul.keller.it@gmail.com>
2018-04-02 17:55:01 +0800

    Changes before Data Remodelling

---
+++ scriptFiles/cddLevelConfiguration.js
@@ -0,0 +1,133 @@
+getCDDLevelData([
+    {
+        "listOption": "Legal Existence",
+        "subMenu": [
+            {
+                "listOption": "Articles of Incorporation"
+            },
+            {
+                "listOption": "Business Activity"
+            },
+            {
+                "listOption": "Nature of Relationship"
+            },
+            {
+                "listOption": "Products List"
+            }
+        ]
+    },
+    {
+        "listOption": "Ownership Structure",
+        "subMenu": [
+            {
+                "listOption": "Identify UBO"
+            },
+            {
+                "listOption": "Significant Shareholders",
+                "subMenu": [
+                    {
+                        "listOption": "Threshold - 3%"
+                    },
+                    {
+                        "listOption": "Threshold - 5%"
+                    },
+                    {
+                        "listOption": "Threshold - 10%"
+                    },
+                    {
+                        "listOption": "Threshold - 15%"
+                    },
+                    {
+                        "listOption": "Threshold - 20%"
+                    },
+                    {
+                        "listOption": "Threshold - 25%"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "listOption": "Principals",
+        "subMenu": [
+            {
+                "listOption": "Main Principals",
+                "subMenu": [
+                    {
+                        "listOption": "CEO"
+                    },
+                    {
+                        "listOption": "CFO"
+                    }
+                ]
+            },
+            {
+                "listOption": "Principals",
+                "subMenu": [
+                    {
+                        "listOption": "Board of Directors"
+                    },
+                    {
+                        "listOption": "Sharia Board"
+                    },
+                    {
+                        "listOption": "Supervisory Board"
+                    }
+                ]
+            },
+            {
+                "listOption": "Pseudo UBO",
+                "subMenu": [
+                    {
+                        "listOption": "1 Main Principal"
+                    },
+                    {
+                        "listOption": "2 Main Principals"
+                    },
+                    {
+                        "listOption": "3 Main Principals"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "listOption": "Legal Representative",
+        "subMenu": [
+            {
+                "listOption": "Require Photo ID"
+            },
+            {
+                "listOption": "Natural Person"
+            }
+        ]
+    },
+    {
+        "listOption": "Screening",
+        "subMenu": [
+            {
+                "listOption": "Internal Name Screening"
+            },
+            {
+                "listOption": "External Name Screening"
+            },
+            {
+                "listOption": "Adverse Intelligence Check"
+            }
+        ]
+    },
+    {
+        "listOption": "Source of Funds/Wealth",
+        "subMenu": [
+            {
+                "listOption": "Source of Capital"
+            },
+            {
+                "listOption": "Source of Income"
+            },
+            {
+                "listOption": "Source of Wealth"
+            }
+        ]
+    }
+]);
