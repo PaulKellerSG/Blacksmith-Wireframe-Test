c7b651c835b8c536e761b480298fd24c894b1ec7
Paul Keller <paul.keller.it@gmail.com>
2018-04-10 21:08:28 +0800

    More drag and drop.

---
+++ html/test.js
@@ -0,0 +1,242 @@
+var data = [{
+                    "parent": "none",
+                    "parentLevel": "0",
+                    "listOptionID": "legalExistence",
+                    "listOption": "Legal Existence",
+                    "subMenu": [
+                        {
+                            "parent": "legalExistence",
+                            "parentLevel": "1",
+                            "listOptionID": "articlesOfIncorporation",
+                            "listOption": "Articles of Incorporation"
+                        },
+                        {
+                            "parent": "legalExistence",
+                            "parentLevel": "1",
+                            "listOptionID": "businessActivity",
+                            "listOption": "Business Activity"
+                        },
+                        {
+                            "parent": "legalExistence",
+                            "parentLevel": "1",
+                            "listOptionID": "natureOfRelationship",
+                            "listOption": "Nature of Relationship"
+                        },
+                        {
+                            "parent": "legalExistence",
+                            "parentLevel": "1",
+                            "listOptionID": "productsList",
+                            "listOption": "Products List"
+                        }
+                    ]
+                },
+                {
+                    "parent": "none",
+                    "parentLevel": "0",
+                    "listOptionID": "ownershipStructure",
+                    "listOption": "Ownership Structure",
+                    "subMenu": [
+                        {
+                            "parent": "ownershipStructure",
+                            "parentLevel": "1",
+                            "listOptionID": "identifyUBO",
+                            "listOption": "Identify UBO"
+                        },
+                        {
+                            "parent": "ownershipStructure",
+                            "parentLevel": "1",
+                            "listOptionID": "significantShareholders",
+                            "listOption": "Significant Shareholders",
+                            "subMenu": [
+                                {
+                                    "parent": "significantShareholders",
+                                    "parentLevel": "2",
+                                    "listOptionID": "3",
+                                    "listOption": "Threshold - 3%"
+                                },
+                                {
+                                    "parent": "significantShareholders",
+                                    "parentLevel": "2",
+                                    "listOptionID": "5",
+                                    "listOption": "Threshold - 5%"
+                                },
+                                {
+                                    "parent": "significantShareholders",
+                                    "parentLevel": "2",
+                                    "listOptionID": "10",
+                                    "listOption": "Threshold - 10%"
+                                },
+                                {
+                                    "parent": "significantShareholders",
+                                    "parentLevel": "2",
+                                    "listOptionID": "15",
+                                    "listOption": "Threshold - 15%"
+                                },
+                                {
+                                    "parent": "significantShareholders",
+                                    "parentLevel": "2",
+                                    "listOptionID": "20",
+                                    "listOption": "Threshold - 20%"
+                                },
+                                {
+                                    "parent": "significantShareholders",
+                                    "parentLevel": "2",
+                                    "listOptionID": "25",
+                                    "listOption": "Threshold - 25%"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "parent": "none",
+                    "parentLevel": "0",
+                    "listOptionID": "principalsGroup",
+                    "listOption": "Principals",
+                    "subMenu": [
+                        {
+                            "parent": "principalsGroup",
+                            "parentLevel": "1",
+                            "listOptionID": "mainPrincipals",
+                            "listOption": "Main Principals",
+                            "subMenu": [
+                                {
+                                    "parent": "mainPrincipals",
+                                    "parentLevel": "2",
+                                    "listOptionID": "ceo",
+                                    "listOption": "CEO"
+                                },
+                                {
+                                    "parent": "mainPrincipals",
+                                    "parentLevel": "2",
+                                    "listOptionID": "cfo",
+                                    "listOption": "CFO"
+                                }
+                            ]
+                        },
+                        {
+                            "parent": "principalsGroup",
+                            "parentLevel": "1",
+                            "listOptionID": "principals",
+                            "listOption": "Principals",
+                            "subMenu": [
+                                {
+                                    "parent": "principals",
+                                    "parentLevel": "2",
+                                    "listOptionID": "boardOfDirectors",
+                                    "listOption": "Board of Directors"
+                                },
+                                {
+                                    "parent": "principals",
+                                    "parentLevel": "2",
+                                    "listOptionID": "shariaBoard",
+                                    "listOption": "Sharia Board"
+                                },
+                                {
+                                    "parent": "principals",
+                                    "parentLevel": "2",
+                                    "listOptionID": "supervisoryBoard",
+                                    "listOption": "Supervisory Board"
+                                }
+                            ]
+                        },
+                        {
+                            "parent": "principalsGroup",
+                            "parentLevel": "1",
+                            "listOptionID": "pseudoUbo",
+                            "listOption": "Pseudo UBO",
+                            "subMenu": [
+                                {
+                                    "parent": "pseudoUBO",
+                                    "parentLevel": "2",
+                                    "listOptionID": "1main",
+                                    "listOption": "1 Main Principal"
+                                },
+                                {
+                                    "parent": "pseudoUBO",
+                                    "parentLevel": "2",
+                                    "listOptionID": "2main",
+                                    "listOption": "2 Main Principals"
+                                },
+                                {
+                                    "parent": "pseudoUBO",
+                                    "parentLevel": "2",
+                                    "listOptionID": "3main",
+                                    "listOption": "3 Main Principals"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "parent": "none",
+                    "parentLevel": "0",
+                    "listOptionID": "legalRep",
+                    "listOption": "Legal Representative",
+                    "subMenu": [
+                        {
+                            "parent": "legalRep",
+                            "parentLevel": "1",
+                            "listOptionID": "photoID",
+                            "listOption": "Require Photo ID"
+                        },
+                        {
+                            "parent": "legalRep",
+                            "parentLevel": "1",
+                            "listOptionID": "natural",
+                            "listOption": "Natural Person"
+                        }
+                    ]
+                },
+                {
+                    "parent": "none",
+                    "parentLevel": "0",
+                    "listOptionID": "screening",
+                    "listOption": "Screening",
+                    "subMenu": [
+                        {
+                            "parent": "screening",
+                            "parentLevel": "1",
+                            "listOptionID": "internal",
+                            "listOption": "Internal Name Screening"
+                        },
+                        {
+                            "parent": "screening",
+                            "parentLevel": "1",
+                            "listOptionID": "external",
+                            "listOption": "External Name Screening"
+                        },
+                        {
+                            "parent": "screening",
+                            "parentLevel": "1",
+                            "listOptionID": "adverse",
+                            "listOption": "Adverse Intelligence Check"
+                        }
+                    ]
+                },
+                {
+                    "parent": "none",
+                    "parentLevel": "0",
+                    "listOptionID": "sourceOfFunds",
+                    "listOption": "Source of Funds/Wealth",
+                    "subMenu": [
+                        {
+                            "parent": "sourceOfFunds",
+                            "parentLevel": "1",
+                            "listOptionID": "capital",
+                            "listOption": "Source of Capital"
+                        },
+                        {
+                            "parent": "sourceOfFunds",
+                            "parentLevel": "1",
+                            "listOptionID": "income",
+                            "listOption": "Source of Income"
+                        },
+                        {
+                            "parent": "sourceOfFunds",
+                            "parentLevel": "1",
+                            "listOptionID": "wealth",
+                            "listOption": "Source of Wealth"
+                        }
+                    ]
+                }];
