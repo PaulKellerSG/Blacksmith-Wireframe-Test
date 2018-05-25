7a3670424d9970677645edfea72229545a75b54b
Paul Keller <paul.keller.it@gmail.com>
2018-05-25 17:08:56 +0800

    SQL Work

--- sql/sp_blacksmith_companyName_GETLIST.sql
+++ sql/sp_blacksmith_companyName_GETLIST.sql
@@ -13,7 +13,9 @@ SELECT
     ,acra.entity_type_description
     ,acra.entity_status_description
     ,SUBSTRING(acra.registration_incorporation_date, 1, 10) as registration_incorporation_date
-    ,acra.block + ' ' + acra.street_name + ', ' + acra.level_no + '-' + acra.unit_no + ' ' + acra.building_name + ', ' + acra.postal_code AS full_address
+    ,acra.block + ' ' + acra.street_name + ', ' as [street_name_concat]
+    ,acra.level_no + '-' + acra.unit_no + ' ' + acra.building_name + ', ' as [building_name_concat]
+    ,acra.postal_code
 FROM allAcraRecords acra
 WHERE acra.entity_name LIKE '%' + @entityName + '%'
 ORDER BY acra.entity_name
